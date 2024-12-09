3027203c661755f11cf08b601e8c42405393fe6a
choice91 <chcoie3927@gmail.com>
2024-12-09 23:06:26 +0900

    Feat: 회원가입 > Validation 추가

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,32 +1,36 @@
 "use client";
 
-import React, { ChangeEvent, useState } from "react";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
 import { FaArrowRight } from "react-icons/fa";
 
 import { useRouter } from "next/navigation";
 
 import Input from "@/components/input";
+import { EmailRegex, PwRegex } from "@/constants/regex";
+
+interface SignUpForm {
+  email: string;
+  pw: string;
+  pwConfirm: string;
+  name: string;
+}
 
 const Page = () => {
   const router = useRouter();
 
-  const [email, setEmail] = useState<string>("");
-  const [pw, setPw] = useState<string>("");
-  const [name, setName] = useState<string>("");
+  const { control, handleSubmit, getValues, watch } = useForm<SignUpForm>({
+    mode: "onBlur",
+    defaultValues: {
+      email: "",
+      pw: "",
+      pwConfirm: "",
+      name: "",
+    },
+  });
 
-  const handleChangeSignUpId = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setEmail(e.target.value);
-  };
-
-  const handleChangeSignUpPw = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setPw(e.target.value);
-  };
-
-  const handleChangeSignUpName = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setName(e.target.value);
+  const handleSignUp = () => {
+    console.log(getValues("email"));
   };
 
   const handleRouteSignUp = () => {
@@ -34,41 +38,133 @@ const Page = () => {
   };
 
   return (
-    <>
+    <form onSubmit={handleSubmit(handleSignUp)}>
       <div
-        className="w-full min-w-500 max-w-500 space-y-2 rounded-20 border-1 bg-anti-flash-white
-          px-20 py-50 shadow-lg"
+        className="w-full min-w-500 space-y-2 rounded-20 border-1 bg-anti-flash-white px-20 py-50
+          shadow-lg"
       >
         <p className="mb-10 text-center text-25 font-bold">SIGN UP</p>
         <div className="flex space-x-1">
-          <Input
-            type="text"
-            className="w-full rounded-b-none"
-            text="이메일"
-            value={email}
-            onChange={handleChangeSignUpId}
+          <Controller
+            name="email"
+            control={control}
+            rules={{
+              required: "이메일을 입력해주세요",
+              pattern: {
+                value: EmailRegex,
+                message: "이메일 형식을 맞춰주세요",
+              },
+              minLength: {
+                value: 1,
+                message: "최소 1글자 이상 입력해주세요",
+              },
+            }}
+            defaultValue=""
+            render={({ field, fieldState }) => (
+              <Input
+                type="text"
+                className="w-full rounded-b-none"
+                text="이메일"
+                helperText={fieldState.error && fieldState.error.message}
+                {...field}
+              />
+            )}
           />
+
           <button
-            className="w-100 rounded-10 border-1 bg-silver-sand transition-colors duration-100
+            className="h-54 w-100 rounded-10 border-1 bg-silver-sand transition-colors duration-100
               ease-in-out hover:bg-gainsboro"
           >
             중복체크
           </button>
         </div>
-        <Input
-          type="text"
-          className="rounded-t-none"
-          text="비밀번호"
-          value={pw}
-          onChange={handleChangeSignUpPw}
+
+        <Controller
+          name="pw"
+          control={control}
+          rules={{
+            required: "비밀번호를 입력해주세요",
+            pattern: {
+              value: PwRegex,
+              message: "비밀번호 형식을 맞춰주세요",
+            },
+            minLength: {
+              value: 1,
+              message: "최소 1글자 이상 입력해주세요",
+            },
+            maxLength: {
+              value: 20,
+              message: "비밀번호는 최대 20글자까지 입력할 수 있습니다",
+            },
+            validate: (value) =>
+              value === watch("pwConfirm") || "비밀번호가 일치하지 않습니다",
+          }}
+          defaultValue=""
+          render={({ field, fieldState }) => (
+            <Input
+              type="text"
+              className="rounded-t-none"
+              text="비밀번호"
+              helperText={fieldState.error && fieldState.error.message}
+              {...field}
+            />
+          )}
         />
-        <Input
-          type="text"
-          className="rounded-t-none"
-          text="이름"
-          value={name}
-          onChange={handleChangeSignUpName}
+
+        <Controller
+          name="pwConfirm"
+          control={control}
+          rules={{
+            required: "비밀번호 확인을 입력해주세요",
+            pattern: {
+              value: PwRegex,
+              message: "비밀번호 형식을 맞춰주세요",
+            },
+            minLength: {
+              value: 1,
+              message: "최소 1글자 이상 입력해주세요",
+            },
+            maxLength: {
+              value: 20,
+              message: "비밀번호 확인은 최대 20글자까지 입력할 수 있습니다",
+            },
+            validate: (value) =>
+              value === watch("pw") || "비밀번호가 일치하지 않습니다",
+          }}
+          defaultValue=""
+          render={({ field, fieldState }) => (
+            <Input
+              type="text"
+              className="rounded-t-none"
+              text="비밀번호 확인"
+              helperText={fieldState.error && fieldState.error.message}
+              {...field}
+            />
+          )}
         />
+
+        <Controller
+          name="name"
+          control={control}
+          rules={{
+            required: "이름을 입력해주세요",
+            minLength: {
+              value: 1,
+              message: "최소 1글자 이상 입력해주세요",
+            },
+          }}
+          defaultValue=""
+          render={({ field, fieldState }) => (
+            <Input
+              type="text"
+              className="rounded-t-none"
+              text="이름"
+              helperText={fieldState.error && fieldState.error.message}
+              {...field}
+            />
+          )}
+        />
+
         <button className="w-full rounded-10 bg-baby-blue-eyes py-10 text-20">
           회원가입
         </button>
@@ -80,7 +176,7 @@ const Page = () => {
           </button>
         </div>
       </div>
-    </>
+    </form>
   );
 };
 
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -7,10 +7,19 @@ interface Props {
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
+  helperText?: string | undefined;
 }
 
 const Input = (props: Props) => {
-  const { className, type = "text", text, value, onChange, onBlur } = props;
+  const {
+    className,
+    type = "text",
+    text,
+    value,
+    onChange,
+    onBlur,
+    helperText,
+  } = props;
 
   return (
     <div className={className}>
@@ -21,22 +30,24 @@ const Input = (props: Props) => {
           value={value}
           onChange={onChange}
           onBlur={onBlur}
-          className="border-silver-sand focus:border-azure dark:bg-sonic-silver peer box-border
-            w-full rounded-10 border-1 bg-white px-16 pb-8 pt-20 transition-colors
-            duration-200 ease-in-out focus:border-1 focus:outline-none dark:text-white"
+          className="focus:border-azure peer box-border w-full rounded-10 border-1 border-silver-sand
+            bg-white px-16 pb-8 pt-20 transition-colors duration-200 ease-in-out
+            focus:border-1 focus:outline-none dark:bg-sonic-silver dark:text-white"
         />
         <label
           htmlFor="input"
-          className="text-sonic-silver peer-focus:-translate-y-20p peer-focus:text-azure
-            peer-[:not(input[value=''])]:-translate-y-20p pointer-events-none absolute
-            bottom-0 left-3 top-0 flex items-center text-15 transition-all duration-200
-            ease-in-out peer-focus:text-12 peer-[:not(input[value=''])]:text-12"
+          className="peer-focus:text-azure pointer-events-none absolute bottom-0 left-3 top-0 flex
+            items-center text-15 text-sonic-silver transition-all duration-200 ease-in-out
+            peer-focus:-translate-y-20p peer-focus:text-12
+            peer-[:not(input[value=''])]:-translate-y-20p
+            peer-[:not(input[value=''])]:text-12"
         >
           <div className="text-philippine-gray bg-transparent px-5 dark:text-anti-flash-white">
             {text}
           </div>
         </label>
       </div>
+      <p className="text-red px-5 text-13">{helperText}</p>
     </div>
   );
 };
