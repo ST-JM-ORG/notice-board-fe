76607eb5e6bc70339704a8ce8193d8ddee16d2a6
choice91 <chcoie3927@gmail.com>
2024-12-13 22:47:11 +0900

    Feat: 로그인/회원가입 화면 레이아웃 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,9 +4,11 @@ const Layout = (props: PropsWithChildren) => {
   const { children } = props;
 
   return (
-    <div className="border-box flex h-screen w-full items-center justify-center">
-      {children}
-    </div>
+    <main className="border-box flex h-screen w-full items-center justify-center">
+      <div className="min-w-500 rounded-20 border-1 bg-anti-flash-white px-20 py-50 shadow-lg">
+        {children}
+      </div>
+    </main>
   );
 };
 
