a43dcc465bd8aae7b7d352b825aeec270c4e5234
choice91 <chcoie3927@gmail.com>
2024-12-14 20:15:24 +0900

    Feat: 로그인/회원가입 레이아웃 background opacity, backdrop-filter 적용

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,10 @@ const Layout = (props: PropsWithChildren) => {
 
   return (
     <main className="border-box flex h-screen w-full items-center justify-center">
-      <div className="min-w-500 rounded-20 border-1 bg-anti-flash-white px-20 py-50 shadow-lg">
+      <div
+        className="min-w-500 rounded-20 border-1 bg-anti-flash-white bg-opacity-30 px-20 py-50
+          shadow-lg backdrop-blur-md"
+      >
         {children}
       </div>
     </main>
