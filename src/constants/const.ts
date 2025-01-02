ed5689c8b330219ede240b5dc78b2258ed1d45d4
choice <chcoie3927@gmail.com>
2025-01-02 10:39:46 +0900

    Feat: 공통 Error resposne object 추가

--- src/constants/const.ts
+++ src/constants/const.ts
@@ -1,3 +1,13 @@
 export const ACCESS_TOKEN = "token";
 
 export const REFRESH_TOKEN = "refresh";
+
+export const ERROR_RESPONSE = {
+  data: null,
+  result: {
+    status: 500,
+    code: "E500",
+    message: "서버에 에러가 발생했습니다. 잠시 후 다시 시도해주세요.",
+    target: null,
+  },
+};
