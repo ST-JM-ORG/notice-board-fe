3c1436df6460640aed8691ecb949bbfae259c526
choice <chcoie3927@gmail.com>
2025-01-06 15:26:09 +0900

    Fix: 로그인 validator class 수정

--- src/models/validator-model.ts
+++ src/models/validator-model.ts
@@ -1,13 +1,13 @@
-import { IsEmail, IsOptional, IsString, Length, Matches, MinLength, ValidateIf } from "class-validator";
+import { IsOptional, Length, Matches, MinLength, ValidateIf } from "class-validator";
 
 import { EmailRegex, PwRegex } from "@/constants/regex";
 
 // 로그인
 export class LoginForm {
-  @IsEmail()
+  @MinLength(1, { message: "이메일을 입력해주세요" })
   email: string;
 
-  @IsString()
+  @MinLength(1, { message: "비밀번호를 입력해주세요" })
   pw: string;
 }
 
